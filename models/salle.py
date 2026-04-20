483151273ad335a3822922a56b0f49519bcd5a9e
Karim <bambakarim1201@gmail.com>
2026-04-20 00:29:00 -0400

    creation des different button et les champ de saisis modification

--- models/salle.py
+++ models/salle.py
@@ -5,5 +5,8 @@ class salle:
         self.type_salle = type_salle
         self.capacite = capacite
 
-    
-       
+    def afficher(self):
+        print(f"Code: {self.code}")
+        print(f"Libellé: {self.libelle}")
+        print(f"Type de salle: {self.type_salle}")
+        print(f"Capacité: {self.capacite}")
