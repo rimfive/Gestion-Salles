8689fe30f5875727e8dc54dcd862e9b9d4a98b52
Karim <bambakarim1201@gmail.com>
2026-04-19 20:53:32 -0400

    Ajout de la classe salle

---
+++ main.py
@@ -0,0 +1,4 @@
+from models.salle import Salle
+
+s = Salle("A1", "Salle info", "Laboratoire", 30)
+s.afficher_infos()
---
+++ models/salle.py
@@ -0,0 +1,12 @@
+class salle:
+    def __init__(self, code, libelle, type_salle, capacite):
+        self.code = code
+        self.libelle = libelle
+        self.type_salle = type_salle
+        self.capacite = capacite
+
+    def afficher(self):
+        print(f"Code: {self.code}")
+        print(f"Libellé: {self.libelle}")
+        print(f"Type de salle: {self.type_salle}")
+        print(f"Capacité: {self.capacite}")
